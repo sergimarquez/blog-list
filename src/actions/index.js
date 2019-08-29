4febec564759464a1c8441dc4736b2d0d41d0f20
Sergi Marquez <sertrack8@gmail.com>
2019-08-29 17:25:17 +0200

    Fix to retrieve users once, and not make multiple calls retrieving the user every time we need it.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,13 @@
+import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  //iterate throught the posts to look for all the users ids and fetch the unique ones
+  const userIds = _.uniq(_.map(getState().posts, "userId"));
+  userIds.forEach(id => dispatch(fetchUser(id)));
+};
 
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get("/posts");
@@ -11,3 +20,13 @@ export const fetchUser = id => async dispatch => {
 
   dispatch({ type: "FETCH_USER", payload: response.data });
 };
+
+// memoize way of solving the issue:
+// export const fetchUser = id => dispatch => {
+//   _fetchUser(id, dispatch);
+// };
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//   dispatch({ type: "FETCH_USER", payload: response.data });
+// });
